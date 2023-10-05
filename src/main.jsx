45d10e9e9e549048b0ae4fe2ccce25e539df9bbf
joilsonlira <joilson.lira@live.com>
2023-10-05 18:24:26 -0300

    Adicionar imports para configuração inicial do React Router DOM
    Este commit adiciona os imports dos seguintes arquivos ao arquivo :
    -  para a página inicial.
    -  para a página de projetos.
    -  para a página de contatos.
    
    Esses imports são parte da configuração inicial do React Router DOM e serão usados para rotear
    entre as diferentes páginas do aplicativo.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Imports das paginas
+import Home from './pages/Home/index.jsx'
+import Projects from './pages/Projects/index.jsx'
+import Contacts from './pages/Contacts/index.jsx'
+
+const router = createBrowserRouter(
+    [
+      {
+        path:"/",
+        element: <Home />
+      },
+      {
+        path:"/projects",
+        element: <Projects />
+      },
+      {
+        path:"/contacts",
+        element: <Contacts />
+      }
+    ]
+  )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
