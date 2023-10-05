0d021be8dd849acb4de88c6d7be1c114a55e7d06
joilsonlira <joilson.lira@live.com>
2023-10-05 19:22:44 -0300

    Mover informações pertinentes à página home para src/Home/index.jsx
    Este commit reorganiza o código para melhorar a estrutura do projeto, movendo as informações específicas à página home do arquivo  para um novo arquivo .
    
    Essa mudança ajuda a manter um código mais organizado e modular, permitindo que as funcionalidades da página home sejam gerenciadas separadamente.
    
    DETALHE ADICIONAL:
    Além de mover o código, esta atualização também atualiza as importações e referências em outros arquivos para refletir a nova localização do componente home. Isso garante que o aplicativo continue funcionando corretamente após a reorganização, sem interrupções para os usuários.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./styles/global.css";
-import j from "./assets/j-icon.png"
 
 // Reaproveitamento de estruturas
 import { Outlet } from "react-router-dom";
@@ -7,7 +6,7 @@ import { Outlet } from "react-router-dom";
 const Nome = "fulano de tal";
 
 function App() {
-
+  
   return (
     <main>
       <header className="main-header">
@@ -27,105 +26,9 @@ function App() {
           </div>  
         </div>
       </header>
-      <Outlet />
-      {/* <section>
-        <div className="content">
-          <figure className="content-foto">
-            <img src="https://img.freepik.com/fotos-premium/feliz-jovem-adulto-afro-americano-andando-no-parque-usando-fones-de-ouvido_113876-3750.jpg" alt="photo" />
-          </figure>
-          <div className="content-info">
-            <div className="description">
-              <h2>Ola!</h2>
-              <p>Me chamo {Nome}. Sou estudante de <b>Analise e desenvolvimento de Sistemas</b>, desenvolvedor <b>Front-end</b>, apaixonado por games e entusiasta no mundo <b>Open Source</b>.</p>
-            </div>
-            <div className="social">
-              <div className="icon"></div>
-              <div className="icon"></div>
-              <div className="icon"></div>
-            </div>
-          </div>
-        </div>
-      </section>
 
-      <section className="second">
-        <div className="content">
-          <div className="content-info">
-            <h2>Sobre mim</h2>
-            <p>
-            Estudo desenvolvimento desde <b>2020</b>, faço faculdade de analise e desenvolvimento de Sistemas, e atualmente busco recolocação profissional. Utilizo em meus projetos principalmente tecnologias para o desenvolvimento front-end, com foco no Javascript, <b>Typescript</b> e <b>React.js</b>.
-            </p>
-            <p>
-            Sou apaixonado por videogames, sempre que posivel estouo ouvindo música, espero um dia ficar em dia com os episodios de One Piece e conseguir usar a tecnologia para ajudar as pessoas.
-            </p>
-            <button>acessar curriculo</button>
-          </div>
-          <div className="content-extra">
-            <h2>Hard skills</h2>
-            <div className="box-extra">
-                <div className="row-language">
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                </div>
-                <div className="row-language">
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                </div>
-                <div className="row-language">
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                  <div className="language">
-                    <div className="icon-language">
-                      <img src="#" alt="language" />
-                    </div>
-                    <p>exemplo text</p>
-                  </div>
-                </div>
-              </div>
-          </div>
-        </div>
-      </section> */}
+      <Outlet />
+      
       <footer>
         <p>{Nome} de tal with <b>React.js &#9889;</b>.</p>
       </footer>
